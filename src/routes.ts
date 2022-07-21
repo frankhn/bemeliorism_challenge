8c540ebbab6359179d3f3872a462b8ef66c029eb
frankhn <harfrank2@gmail.com>
2022-07-21 15:01:17 +0200

    feat: adds preferences route
    #BEM-CHALLENGE-1

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import docs from './swagger/v1';
 import auth from './resources/v1/auth/auth.routes'
+import preferences from './resources/v1/preference/preference.routes'
 
 const routes = Router();
 
 routes.use('/docs', swaggerUI.serve, swaggerUI.setup(docs));
 
 routes.use('/auth', auth);
-routes.use('/preference', auth);
+routes.use('/preferences', preferences);
 
 export default routes;
